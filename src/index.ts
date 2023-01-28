122cfb8652d1dbb48f7d2f9ef0f1d10aab46386b
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2023-01-28 16:52:23 -0600

    Implemented simple express server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-// eslint-disable-next-line no-console
-console.log('Hello world!');
+import express from 'express';
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+const app = express();
+app.get('/', (_, res) => {
+  res.send('HELLO WORLD');
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
